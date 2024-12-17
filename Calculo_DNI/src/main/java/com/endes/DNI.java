34be2827617f8f4515343e4d894f4e6947272e20
JuanJoseFernandez10 <fernandez.suarez.juan.jose@iescamas.es>
2024-12-17 12:49:30 +0100

    1escritura

---
+++ Calculo_DNI/src/main/java/com/endes/DNI.java
@@ -0,0 +1,15 @@
+package com.endes;
+
+public class DNI {
+	String DNI;
+	String nombre;
+	
+	public DNI(String nombre, String DNI) {
+		if(DNI.length() == 9) {
+			this.DNI = "";
+		}else {
+			
+		}
+		this.nombre = "";
+	}
+}
