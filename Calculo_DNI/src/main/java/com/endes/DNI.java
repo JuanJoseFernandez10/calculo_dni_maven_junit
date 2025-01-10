95303e8f5228522796521d1f7fe3b5d592623c28
JuanJoseFernandez10 <fernandez.suarez.juan.jose@iescamas.es>
2025-01-10 11:14:52 +0100

    Arreglo de codigo, comienzo de pruebas unitarias

--- Calculo_DNI/src/main/java/com/endes/DNI.java
+++ Calculo_DNI/src/main/java/com/endes/DNI.java
@@ -9,23 +9,33 @@ public class DNI {
 		this.DNI = "";
 		this.nombre = "";
 	}
-	
+	/**
+	 * Calcula la letra del DNI correspondiente a un numero de DNI
+	 * @param DNI
+	 * @return Resultado = La letra del DNI
+	 */
 	public boolean validacion(String DNI) {
 		boolean resultado = false;
-		if(DNI.length() == 9) {
 		String numerosDNI = "";
 		int numero_DNI = 0;
 		for(int i = 0; i < DNI.length() - 1; i++) {
 			numerosDNI += DNI.charAt(i); 
 			 
 		}
-		numero_DNI = Integer.parseInt(numerosDNI);
+		
+		if(DNI.length() == 9 || DNI != null || numerosDNI.matches("\\d+")) {
+		
 		char letraDNI = DNI.charAt(8); 
+		numero_DNI = Integer.parseInt(numerosDNI);
 		
 		char[] letras_DNI = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
 		int resto_validacion = numero_DNI % 23;
+		
 		if(letraDNI == letras_DNI[resto_validacion]) {
 			resultado = true;
+		}else {
+			
+			throw new  IllegalArgumentException("El numero del DNI no es valido");
 		}
 		
 		}//Condition of length
